9899c77f03a0d6de77b7872df6cb465f0f2e6ccb
Hariharan Selvaraj <hariharan.2309.s@icloud.com>
2026-03-06 22:39:13 +0530

    [chore] -- prettier fixes

--- apps/web/components/top-nav.tsx
+++ apps/web/components/top-nav.tsx
@@ -171,7 +171,6 @@ export function TopNav({ user }: TopNavProps) {
               <ThemeIcon className="h-4 w-4" />
             </button>
 
-
             {/* Profile dropdown trigger */}
             <div className="relative">
               <button
